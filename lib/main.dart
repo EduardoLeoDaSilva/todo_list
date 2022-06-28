ab2576b560d24fca42ef3211c93d51edfffece15
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-28 17:41:47 -0300

    Creation of homepage and added textField and a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_list/pages/home_page.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    home: HomePage,
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
   ));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,39 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('data');
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: const Text('Todo item'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(15)),
+                  child: const Icon(Icons.add),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
